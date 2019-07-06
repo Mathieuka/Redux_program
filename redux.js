d1710fcfc7299ee7703c66ad7bc9695203bb836e
Mathieu Kahlaoui <mathieukahlaoui@MacBook-Pro-de-Adminsqli.local>
2019-07-06 12:55:16 +0200

    simulate redux program done

---
+++ redux.js
@@ -0,0 +1,87 @@
+const ADD_POINT = "ADD_POINT";
+const REMOVE_POINT = "REMOVE_POINT";
+const CHANGE_NAME = "CHANGE_NAME";
+
+const addPoint = (state = {}, action) => {
+    switch(action.type){
+        case ADD_POINT: 
+             console.log("addPoint reducer triggered");
+             return {
+                 points: state.points++,
+                 ...state,
+                 };
+        default :
+            return state;
+    }
+    
+}
+
+const removePoint = (state = {}, action) => {
+    switch(action.type){
+        case REMOVE_POINT:
+             console.log("removePoint reducer triggered");
+             return {
+                  points: state.points--,
+                 ...state
+             }
+        default:
+            return state;
+    }
+}
+
+const changeName = (state = {}, action) => {
+    switch(action.type){
+        case CHANGE_NAME:
+             console.log("changeName reducer triggered");
+              state.name = action.name
+             return {
+                 ...state
+             }
+    }
+}
+
+function combineReducer(reducers){
+
+    let state = {
+        ...reducers
+    }
+    return {
+        createStore : (reducers) =>{
+            let listeners = [];
+        
+            const dispatch = action => {       // At every action, dispatch is triggered in every reducer 
+                state = reducers.map(reducer => reducer(state,action))[0]; 
+                listeners.forEach(listener => listener()); 
+            };
+        
+            const getState = () => console.log(state);
+        
+            const subscribe = listener => {
+                listeners.push(listener);
+                return () => {
+                    listeners.filter(l => l !== listener);
+                }
+            }
+        
+            return {
+                getState,
+                dispatch,
+                subscribe
+            }
+        }
+    } 
+}
+
+const subscribe = [
+    addPoint,
+    removePoint,
+    changeName]
+
+const store = combineReducer({ points: 0, name: "" }).createStore(subscribe);
+
+store.dispatch({ type: "ADD_POINT"});
+store.dispatch({ type: "ADD_POINT"});
+store.dispatch({type: "REMOVE_POINT"});
+store.dispatch({type: "CHANGE_NAME", name: "Pierre"});
+store.getState();
+
